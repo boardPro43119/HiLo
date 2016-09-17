3ce390d2c56df3e3b61e6b29be98a4b7f4edce3d
Benjamin Prud'homme <bgprudhomme@gmail.com>
2016-09-17 15:11:51 -0400

    Initialize variables only once

--- HiLo.java
+++ HiLo.java
@@ -4,44 +4,42 @@ import java.util.Scanner;
 public class HiLo {
 	public static void main(String[] args){
 		boolean running = true,
-				correct = false;
+				correct;
 		int target,
-			guess = 100,
-			attempts = 0;
+			guess,
+			attempts;
 		String playAgain = "";
 		Scanner in = new Scanner(System.in);
 		Random rand = new Random();
-			while(running){
-				target = rand.nextInt(100)+1;
-				System.out.println("I'm thinking of a number between 1 and 100.  Guess what it is.");
-				System.out.print("Enter your first guess (or 0 to quit): ");
-				while(!correct){
-					guess = in.nextInt();
-					attempts++;
-					if(guess == 0){
-						System.out.println("The correct number was " + target + ".");
-						break;
-					}
-					else if(guess<target){
-						System.out.print("Too low, try again: ");
-					}
-					else if(guess>target){
-						System.out.print("Too high, try again: ");
-					}
-					else {
-						correct = true;
-						System.out.println(target + " is correct!  You got it in " + attempts + " guesses.");
-					}
+		while(running){
+			correct = false;
+			attempts = 0;
+			target = rand.nextInt(100)+1;
+			System.out.println("I'm thinking of a number between 1 and 100.  Guess what it is.");
+			System.out.print("Enter your first guess (or 0 to quit): ");
+			while(!correct){
+				guess = in.nextInt();
+				attempts++;
+				if(guess == 0){
+					System.out.println("The correct number was " + target + ".");
+					break;
 				}
-				System.out.print("Do you want to play again (Y/N)? ");
-				playAgain = in.next();
-				if(playAgain.equals("N") || playAgain.equals("n")){
-					running = false;
+				else if(guess<target){
+					System.out.print("Too low, try again: ");
 				}
-				else{
-					correct = false;
-					attempts = 0;
+				else if(guess>target){
+					System.out.print("Too high, try again: ");
+				}
+				else {
+					correct = true;
+					System.out.println(target + " is correct!  You got it in " + attempts + " guesses.");
 				}
 			}
+			System.out.print("Do you want to play again (Y/N)? ");
+			playAgain = in.next();
+			if(playAgain.equals("N") || playAgain.equals("n")){
+				running = false;
+			}
+		}
 	}
 }
